acb7a937d5fdd5223aa454e3e47d63c4d19d1f8e
Giovanni Luro <giovanni.luro@outlook.com>
2020-07-05 14:22:18 -0300

    new product placeholders

--- src/modals/NewProduct/index.js
+++ src/modals/NewProduct/index.js
@@ -23,8 +23,8 @@ const NewProduct = ({ setShowAddProduct, setItens, itens }) => {
         <button onClick={() => setShowAddProduct(false)}> <FiXCircle size={28} color="white"/> </button>
         <form onSubmit={handleSubmit}>
           <p>Adicionar novo item</p>
-          <input autofocus type="text" value={nome} onChange={e => setNome(e.target.value)} />
-          <input type="text" value={quantidade} onChange={e => setQuantidade(e.target.value)} />
+          <input autofocus type="text" value={nome} onChange={e => setNome(e.target.value)} placeholder="Descrição" />
+          <input type="text" value={quantidade} onChange={e => setQuantidade(e.target.value)} placeholder="Quantidade"/>
           <button type="submit">Adicionar</button>
         </form>
       </div>
